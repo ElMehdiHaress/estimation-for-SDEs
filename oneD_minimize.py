eb64f528fd6d354f1f1549af823f9b8171448b77
El Mehdi Haress <el-mehdi.haress@student.ecp.fr>
2021-08-11 19:44:00 +0100

    Update oneD_minimize.py

--- oneD_minimize.py
+++ oneD_minimize.py
@@ -2,6 +2,15 @@ from scipy.optimize import minimize
 from oneD_functional import functional_theta
 
 def minimize1D(x_0,Method,Bounds,arguments):
+  '''
+  Runs the minimization procedure and computes 100 realizations of the estimator
+  
+  args:
+      x_0: inital point to start the minimization procedure (float)
+      Method: Algorithm used for the minimization procedure (string)
+      Bounds: Bounds on the parameter we want to estimate
+      arguments: arguments for the function 'functional_theta' which contain the sample, the other known parameters
+  '''
   list_theta = []
   for i in tqdm(range(100)):
       res_theta = minimize(functional_theta, x0= x_0, args = arguments, method=Method, bounds= Bounds, tol = 1e-3)
