e0d331ad9eedf35a24fa8abd1ed46d77513934d6
El Mehdi Haress <el-mehdi.haress@student.ecp.fr>
2021-08-11 18:59:46 +0100

    Update WasserteinDistance.py

--- WasserteinDistance.py
+++ WasserteinDistance.py
@@ -2,4 +2,11 @@ import sys
 !{sys.executable} -m pip install pyemd
 from pyemd import emd_samples
 def wassertein1(x,y):
+    '''
+    Computes the Wassertein distance between two samples x and y
+    
+    args:
+        x:     first sample (array)
+        y:     second sample (array)
+    '''
     return emd_samples(x,y,bins='auto')
