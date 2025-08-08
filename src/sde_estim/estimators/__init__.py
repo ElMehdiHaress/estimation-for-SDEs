ddfd9ad97b99af9d0e5996d1fc066d3c213e55d1
El Mehdi Haress <el-mehdi.haress@centralesupelec.fr>
2025-08-08 23:45:51 +0100

    chore: blank estimators __init__; no public re-exports yet

--- src/sde_estim/estimators/__init__.py
+++ src/sde_estim/estimators/__init__.py
@@ -1,15 +1,1 @@
-"""
-Public API for sde_estim.estimators
-"""
-
-# ré-exporte les fonctions les plus utiles ; adapte si nécessaire
-from .one_d_procedure   import estimate_theta       # noqa: F401
-from .two_d_procedure   import estimate_theta_sigma # noqa: F401
-from .three_d_procedure import estimate_all         # noqa: F401
-
-__all__ = [
-    "estimate_theta",
-    "estimate_theta_sigma",
-    "estimate_all",
-]
-
+pass
